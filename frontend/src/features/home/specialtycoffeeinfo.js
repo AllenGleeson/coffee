b090f2f6de3bb6b30e70771f43c5935172ff77d1
AllenGleeson <allen_gleeson@hotmail.com>
2023-11-14 00:44:05 +0000

    Added styling for brand video description.

--- frontend/src/features/home/specialtycoffeeinfo.js
+++ frontend/src/features/home/specialtycoffeeinfo.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import '../../assets/css/brandvideo.css'
 
 const SpecialtyCoffeeInfo = ({ heading, description }) => {
   return (
     <div className= "col-6">
-      <h2>{heading}</h2>
-      <p>{description}</p>
+      <div className='video-brand'>
+        <h2>{heading}</h2>
+        <hr></hr>
+        <p>{description}</p>
+      </div>
     </div>
   );
 };
