1b6b0caa4fa11b897d3aaa6d57c41b47a6a52860
DiarmaidB <diarmaidbarron@gmail.com>
2024-03-27 23:03:07 +0000

    Adding a custom image for the Map Marker on Google Maps.

--- frontend/src/features/home/map.js
+++ frontend/src/features/home/map.js
@@ -1,6 +1,8 @@
 import React, { useMemo } from "react";
 import { GoogleMap, Marker } from "@react-google-maps/api";
 
+import image from '../../assets/images/MapsMarker.png';
+
 function Map({ lat, lng }) {
   const center = useMemo(() => ({ lat, lng }), [lat, lng]);
   return (
@@ -8,7 +10,7 @@ function Map({ lat, lng }) {
       <Marker 
         position={center}
         icon={{
-          url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png', // Specify the URL of your custom icon
+          url: image, // Specify the URL of your custom icon
           scaledSize: new window.google.maps.Size(40, 40), // Adjust the size if necessary
         }}
       />
