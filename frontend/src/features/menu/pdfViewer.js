90f2815ebcf9765e9bb886e3169e564b9efcc6f2
DiarmaidB <diarmaidbarron@gmail.com>
2024-04-06 14:02:13 +0100

    Changed the PDF Viewer to display all pages of the PDF file.

--- frontend/src/features/menu/pdfViewer.js
+++ frontend/src/features/menu/pdfViewer.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 
 // Specify the URL to the worker script
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PDFViewer = ({ pdf }) => {
+  const [numPages, setNumPages] = useState(null);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
+  const renderPages = () => {
+    const pages = [];
+    for (let pageNumber = 1; pageNumber <= numPages; pageNumber++) {
+      pages.push(
+        <Page 
+          key={pageNumber} 
+          pageNumber={pageNumber} 
+          renderTextLayer={false} 
+          renderAnnotationLayer={false} 
+        />
+      );
+    }
+    return pages;
+  };
+
   return (
     <>
       {pdf ? (
-        <Document file={pdf}>
-        <Page pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} />
-      </Document>      
+        <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+          {numPages && renderPages()}
+        </Document>      
       ) : (
         <div>No PDF file specified.</div>
       )}
